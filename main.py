271b65d32a792fde84a6587e38fa612cd16b77cc
Tanapuch <nik.muic@gmail.com>
2025-04-19 17:43:40 +0700

    init: initialize the sandbox

---
+++ main.py
@@ -0,0 +1,28 @@
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.documents import Document
+
+# Initialize with an embedding model
+vector_store = InMemoryVectorStore(embedding=OpenAIEmbeddings())
+
+document_1 = Document(
+    page_content="I had chocalate chip pancakes and scrambled eggs for breakfast this morning.",
+    metadata={"source": "tweet"},
+)
+
+document_2 = Document(
+    page_content="The weather forecast for tomorrow is cloudy and overcast, with a high of 62 degrees.",
+    metadata={"source": "news"},
+)
+
+documents = [document_1, document_2]
+
+vector_store.add_documents(documents=documents, ids=["doc1", "doc2"])
+
+top_n = 10
+for index, (id, doc) in enumerate(vector_store.store.items()):
+    if index < top_n:
+        # docs have keys 'id', 'vector', 'text', 'metadata'
+        print(f"{id}: {doc['text']}")
+    else:
+        break
